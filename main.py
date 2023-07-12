4f38cc0bba2b4d87b5b66a99c235cdca29025e00
cvele3 <cvetkojakov@gmail.com>
2023-07-12 20:29:21 +0200

    final version

--- main.py
+++ main.py
@@ -8,10 +8,9 @@
 #    pip.main(['install', path])
 import math
 
-#install_whl(tensorinjo)
-#install_whl(rdkitinjo)
-#install_whl(stelargrfinjo)
-
+# install_whl(tensorinjo)
+# install_whl(rdkitinjo)
+# install_whl(stelargrfinjo)
 
 
 from stellargraph.mapper import PaddedGraphGenerator
@@ -23,7 +22,6 @@ from stellargraph import datasets
 from sklearn import model_selection
 from stellargraph import StellarGraph
 from stellargraph.mapper import PaddedGraphGenerator
-
 
 from tensorflow.keras import Model
 from tensorflow.keras.optimizers import Adam
@@ -50,10 +48,8 @@ from tensorflow.keras import Model, optimizers, losses, metrics
 import numpy as np
 import pandas as pd
 
-
 filepath_raw = 'avpdb_smiles.xlsx'
 data_file = pd.read_excel(filepath_raw, header=0, usecols=["smiles", "label"])
-
 
 listOfTuples = []
 
@@ -68,18 +64,37 @@ ZeroActivity = 0
 OneActivity = 0
 stellarGraphAllList = []
 
-
 from collections import Counter
 
-# Get a list of all unique elements in the molecules
+# Get a list of all unique elements in the molecules and extract features.
+# At the same time, create a list of seen properties from each atom.
 all_elements = []
+lst_degree = []
+lst_formal_charge = []
+lst_radical_electrons = []
+lst_hybridization = []
+lst_aromatic = []
 for molecule in listOfTuples:
     mol = Chem.MolFromSmiles(molecule[0])
     atoms = mol.GetAtoms()
     for atom in atoms:
+        lst_degree.append(atom.GetDegree())
+        lst_formal_charge.append(atom.GetFormalCharge())
+        lst_radical_electrons.append(atom.GetNumRadicalElectrons())
+        lst_hybridization.append(atom.GetHybridization().real)
+        lst_aromatic.append(atom.GetIsAromatic())
+
         element = atom.GetSymbol()
         if element not in all_elements:
             all_elements.append(element)
+
+# Determine min and max values for each property/feature.
+# This values will be later used for min-max scaling.
+min_degree, max_degree = min(lst_degree), max(lst_degree)
+min_formal_charge, max_formal_charge = min(lst_formal_charge), max(lst_formal_charge)
+min_radical_electrons, max_radical_electrons = min(lst_radical_electrons), max(lst_radical_electrons)
+min_hybridization, max_hybridization = min(lst_hybridization), max(lst_hybridization)
+min_aromatic, max_aromatic = min(lst_aromatic), max(lst_aromatic)
 
 # Create a dictionary that maps each element to a unique index
 element_to_index = {element: index for index, element in enumerate(all_elements)}
@@ -101,15 +116,15 @@ for molecule in listOfTuples:
     node_features = []
     for atom in atoms:
         element = atom.GetSymbol()
-        degree = atom.GetDegree()
-        formal_charge = atom.GetFormalCharge()
-        num_radical_electrons = atom.GetNumRadicalElectrons()
-        hybridization = atom.GetHybridization().real
-        aromatic = atom.GetIsAromatic()
+        degree = (atom.GetDegree() - min_degree) / (max_degree - min_degree)
+        # formal_charge = (atom.GetFormalCharge() - min_formal_charge) / (max_formal_charge - min_formal_charge)
+        # num_radical_electrons = (atom.GetNumRadicalElectrons() - min_radical_electrons) / (max_radical_electrons - min_radical_electrons)
+        hybridization = (atom.GetHybridization().real - min_hybridization) / (max_hybridization - min_hybridization)
+        aromatic = (atom.GetIsAromatic() - min_aromatic) / (max_aromatic - min_aromatic)
         element_onehot = [0] * len(all_elements)
         element_onehot[element_to_index[element]] = 1
-        node_features.append(element_onehot)
-        #node_features.append(element_onehot + [degree, formal_charge, num_radical_electrons, hybridization, aromatic])
+        #node_features.append(element_onehot)
+        node_features.append(element_onehot + [degree, hybridization, aromatic])
     node_features = np.array(node_features)
 
     # Convert the edges to a pandas DataFrame
@@ -118,24 +133,20 @@ for molecule in listOfTuples:
     # Create a StellarGraph object from the molecular graph
     G = StellarGraph(nodes=node_features, edges=edges_df)
 
-
     if smileLabel == 1 and OneActivity < 1000:
-      OneActivity += 1
-      skup = (G, smileLabel)
-      stellarGraphAllList.append(skup)
+        OneActivity += 1
+        skup = (G, smileLabel)
+        stellarGraphAllList.append(skup)
 
     if smileLabel == 0 and ZeroActivity < 1000:
         ZeroActivity += 1
         skup = (G, smileLabel)
         stellarGraphAllList.append(skup)
 
-
 print(ZeroActivity)
 print(OneActivity)
 
 print(len(stellarGraphAllList))
-
-
 
 import pandas as pd
 
@@ -145,11 +156,10 @@ graphs = []
 labels = []
 
 for triple in stellarGraphAllList:
-  grafinjo = triple[0]
-  active = triple[1]
-  graphs.append(grafinjo)
-  labels.append(active)
-
+    grafinjo = triple[0]
+    active = triple[1]
+    graphs.append(grafinjo)
+    labels.append(active)
 
 import pandas as pd
 
@@ -159,7 +169,7 @@ graph_labels = pd.Series(labels)
 
 print(graph_labels.value_counts().to_frame())
 
-#graph_labels = pd.get_dummies(graph_labels, drop_first=True)
+# graph_labels = pd.get_dummies(graph_labels, drop_first=True)
 generator = PaddedGraphGenerator(graphs=graphs)
 
 import numpy as np
@@ -172,12 +182,11 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.callbacks import LambdaCallback
 from tensorflow.keras.utils import Sequence
 
-
 epochs = 10000
 
 # Define the number of rows for the output tensor and the layer sizes
-k = 30
-layer_sizes = [30, 30, 30, 1]
+k = 15
+layer_sizes = [10, 10, 10, 1]
 
 # Create the DeepGraphCNN model
 dgcnn_model = DeepGraphCNN(
@@ -189,15 +198,15 @@ dgcnn_model = DeepGraphCNN(
 )
 x_inp, x_out = dgcnn_model.in_out_tensors()
 
-x_out = Conv1D(filters=32, kernel_size=sum(layer_sizes), strides=sum(layer_sizes))(x_out)
+x_out = Conv1D(filters=8, kernel_size=sum(layer_sizes), strides=sum(layer_sizes))(x_out)
 x_out = MaxPool1D(pool_size=2)(x_out)
 
-x_out = Conv1D(filters=64, kernel_size=5, strides=1)(x_out)
+x_out = Conv1D(filters=16, kernel_size=5, strides=1)(x_out)
 
 x_out = Flatten()(x_out)
 
-x_out = Dense(units=128, activation="relu")(x_out)
-x_out = Dropout(rate=0.5)(x_out)
+x_out = Dense(units=64, activation="relu")(x_out)
+x_out = Dropout(rate=0.2)(x_out)
 
 predictions = Dense(units=1, activation="sigmoid")(x_out)
 
@@ -226,6 +235,7 @@ def roc_auc_metric(y_true, y_pred):
     print("ROC AUC: ", roc_auc)
     roc_auc_values.append(K.get_value(roc_auc))
 
+
 def rest_of_metrics(y_true, y_pred):
     y_pred = K.cast(K.round(y_pred), K.floatx())
     y_true = K.cast(y_true, K.floatx())
@@ -250,7 +260,6 @@ def rest_of_metrics(y_true, y_pred):
     recall = recall_score(y_true, y_pred)
     f1 = f1_score(y_true, y_pred)
 
-
     print("GM: ", gm)
     print("Precision: ", precision)
     print("Recall: ", recall)
@@ -264,11 +273,11 @@ def rest_of_metrics(y_true, y_pred):
     tpr_values.append(K.get_value(tpr))
 
 
-restOfMetrics_callback = LambdaCallback(on_epoch_end=lambda epoch, logs: metrics(test_gen.targets, model.predict(test_gen)))
-
-
+restOfMetrics_callback = LambdaCallback(
+    on_epoch_end=lambda epoch, logs: metrics(test_gen.targets, model.predict(test_gen)))
 
 mcc_values = []
+
 
 # Create a LambdaCallback to calculate MCC at the end of each epoch
 def mcc_metric(y_true, y_pred):
@@ -278,6 +287,7 @@ def mcc_metric(y_true, y_pred):
     mcc = matthews_corrcoef(y_true, y_pred)
     print("MCC: ", mcc)
     mcc_values.append(mcc)
+
 
 mcc_callback = LambdaCallback(on_epoch_end=lambda epoch, logs: mcc_metric(test_gen.targets, model.predict(test_gen)))
 callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=15, restore_best_weights=True)
@@ -297,9 +307,6 @@ model.compile(
 # model.metrics_names.append('recall')
 # model.metrics_names.append('f1')
 # model.metrics_names.append('roc_auc')
-
-
-
 
 
 cv = StratifiedKFold(n_splits=10, shuffle=True)
@@ -345,7 +352,6 @@ for train_index, test_index in cv.split(graphs, graph_labels):
         train_gen, epochs=epochs, verbose=1, validation_data=val_gen, shuffle=True, callbacks=[callback]
     )
 
-
     histories.append(history)
 
     y_pred = model.predict(test_gen)
@@ -359,31 +365,26 @@ for train_index, test_index in cv.split(graphs, graph_labels):
     rest_of_metrics(y_test, y_pred)
     mcc_metric(y_test, y_pred)
 
-
 import os
 import matplotlib.pyplot as plt
 import stellargraph as sg
 
-#save_dir = r"C:\Users\jcvetko\Desktop\stuff\school\6. semestar\Zavrsni rad\plotHistory 444-598 split with k30 [30,30,30,1]"
+# save_dir = r"C:\Users\jcvetko\Desktop\stuff\school\6. semestar\Zavrsni rad\plotHistory 444-598 split with k30 [30,30,30,1]"
 # save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\plotHistory 444-598 split with k35 [32,32,32,1]_history"
 # save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\testDir"
 
-# save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\newMetrics\history\plotHistory 444-598 split with k35 [32,32,32,1] with 32- 64- 128"
-# save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\newMetrics\history\plotHistory 444-598 split with k35 [32,32,32,1] with 16 -32 -128"
-# save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\newMetrics\history\plotHistory 444-598 split with k30 [30,30,30,1] with 16 -32 -128"
-# save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\newMetrics\history\plotHistory 444-598 split with k25 [25,25,25,1] with 16 -32 -128"
-# save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\newMetrics\history\plotHistory 444-598 split with k25 [25,25,25,1] with 8 -16 -64"
-save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\newMetrics\history\plotHistory 444-598 split with k20 [15,15,15,1] with 8 -16 -64"
-# save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\newMetrics\history\plotHistory 444-598 split with k15 [10,10,10,1] with 8 -16 -64"
+# save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\updatedNodeFeaturesWithFeatures\history\plotHistory 444-598 split with k35 [32,32,32,1] with 32- 64- 128"
+# save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\updatedNodeFeaturesWithFeatures\history\plotHistory 444-598 split with k35 [32,32,32,1] with 16 -32 -128"
+# save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\updatedNodeFeaturesWithFeatures\history\plotHistory 444-598 split with k30 [30,30,30,1] with 16 -32 -128"
+# save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\updatedNodeFeaturesWithFeatures\history\plotHistory 444-598 split with k25 [25,25,25,1] with 16 -32 -128"
+# save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\updatedNodeFeaturesWithFeatures\history\plotHistory 444-598 split with k25 [25,25,25,1] with 8 -16 -64"
+# save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\updatedNodeFeaturesWithFeatures\history\plotHistory 444-598 split with k20 [15,15,15,1] with 8 -16 -64"
+save_dir = r"C:\Users\legion\PycharmProjects\PeptideML\zavrsni\updatedNodeFeaturesWithFeatures\history\plotHistory 444-598 split with k15 [10,10,10,1] with 8 -16 -64"
 
 for i, history in enumerate(histories):
     fig = sg.utils.plot_history(history, individual_figsize=(7, 4), return_figure=True)
     fig.savefig(os.path.join(save_dir, f"history_{i}.png"))
     plt.close(fig)
-
-
-
-
 
 test_metrics = model.evaluate(test_gen)
 print("\nTest Set Metrics:")
@@ -411,13 +412,13 @@ data = {
 # Create a pandas DataFrame from the dictionary
 df = pd.DataFrame(data)
 
-# excel_save = "zavrsni/newMetrics/metrics/plotHistory 444-598 split with k35 [32,32,32,1] with 32- 64- 128/"
-# excel_save = "zavrsni/newMetrics/metrics/plotHistory 444-598 split with k35 [32,32,32,1] with 16 -32 -128/"
-# excel_save = "zavrsni/newMetrics/metrics/plotHistory 444-598 split with k30 [30,30,30,1] with 16 -32 -128/"
-# excel_save = "zavrsni/newMetrics/metrics/plotHistory 444-598 split with k25 [25,25,25,1] with 16 -32 -128/"
-# excel_save = "zavrsni/newMetrics/metrics/plotHistory 444-598 split with k25 [25,25,25,1] with 8 -16 -64/"
-excel_save = "zavrsni/newMetrics/metrics/plotHistory 444-598 split with k20 [15,15,15,1] with 8 -16 -64/"
-# excel_save = "zavrsni/newMetrics/metrics/plotHistory 444-598 split with k15 [10,10,10,1] with 8 -16 -64/"
+# excel_save = "zavrsni/updatedNodeFeaturesWithFeatures/metrics/plotHistory 444-598 split with k35 [32,32,32,1] with 32- 64- 128/"
+# excel_save = "zavrsni/updatedNodeFeaturesWithFeatures/metrics/plotHistory 444-598 split with k35 [32,32,32,1] with 16 -32 -128/"
+# excel_save = "zavrsni/updatedNodeFeaturesWithFeatures/metrics/plotHistory 444-598 split with k30 [30,30,30,1] with 16 -32 -128/"
+# excel_save = "zavrsni/updatedNodeFeaturesWithFeatures/metrics/plotHistory 444-598 split with k25 [25,25,25,1] with 16 -32 -128/"
+# excel_save = "zavrsni/updatedNodeFeaturesWithFeatures/metrics/plotHistory 444-598 split with k25 [25,25,25,1] with 8 -16 -64/"
+# excel_save = "zavrsni/updatedNodeFeaturesWithFeatures/metrics/plotHistory 444-598 split with k20 [15,15,15,1] with 8 -16 -64/"
+excel_save = "zavrsni/updatedNodeFeaturesWithFeatures/metrics/plotHistory 444-598 split with k15 [10,10,10,1] with 8 -16 -64/"
 
 # Save the DataFrame to an Excel file
-df.to_excel( excel_save + "metrics.xlsx", index=False)
+df.to_excel(excel_save + "metrics.xlsx", index=False)
